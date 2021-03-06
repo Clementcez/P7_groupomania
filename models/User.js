422e165b0a516152b52491be29987ed216d529df
Clementcez <couillaud11000@gmail.com>
2021-03-06 09:07:23 +0100

    création backend

---
+++ models/User.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+    });
+
+    return User;
+};
---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const db = require("./models");
+
+
+db.sequelize.sync()
+.then((req) => {
+    app.listen(3000, () =>{
+        console.log("server running");
+    });
+});
